285cd3563956d59c4ad45d69b789eb8e8e107471
jiacongluo <1215752124@qq.com>
2020-05-31 22:01:39 +0800

    Do exercises!
    Signed-off-by: jiacongluo <1215752124@qq.com>

--- src/com/Jiacong/Main.java
+++ src/com/Jiacong/Main.java
@@ -1,4 +1,5 @@
 package com.Jiacong;
+
 import com.Jiacong.Array.*;
 
 import java.util.Arrays;
@@ -8,10 +9,13 @@ public class Main {
     public static void main(String[] args) {
         // 算法测试函数
         ArrayAlgorithm test = new ArrayAlgorithm();
-        int[] nums1 = {1, 2, 2, 1};
-        int[] nums2 = {2, 2};
+        int[] nums1 = {1, 2, 2, 9};
+        int[] nums2 = {9, 9};
 
-        int[] answer = test.intersection(nums1, nums2);
+//        int[] answer = test.intersection(nums1, nums2);
+        int[] answer = test.plusOne(nums2);
         System.out.println(Arrays.toString(answer));
+        while (true) {
+        }
     }
 }
